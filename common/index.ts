551c879b00aab2ec77c1fa8284f93bd243ee7daf
SauceCode84 <rayruok@gmail.com>
2017-07-06 09:21:13 +0200

    created Publisher

--- common/index.ts
+++ common/index.ts
@@ -1,6 +1,7 @@
 
 export * from "./consumer";
 export * from "./producer";
+export * from "./publisher";
 export * from "./requester";
 export * from "./responder";
 export * from "./types";
---
+++ common/publisher.ts
@@ -0,0 +1,16 @@
+import * as amqp from "amqplib";
+
+export class Publisher {
+
+  private name: string;
+  private exchangeName: string;
+
+  private connection: amqp.Connection;
+  private channel: amqp.Channel;
+
+  constructor({ name, exchangeName }: { name: string, exchangeName: string }) {
+    this.name = name;
+    this.exchangeName = exchangeName;
+  }
+
+}
