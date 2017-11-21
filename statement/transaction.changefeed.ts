c067f88b5b4cbc8b01879b26e362634d3bb2762d
SauceCode84 <rayruok@gmail.com>
2017-11-21 09:03:39 +0200

    cleaned up transaction change feed

--- statement/transaction.changefeed.ts
+++ statement/transaction.changefeed.ts
@@ -2,14 +2,12 @@ import * as r from "rethinkdb";
 import * as Decimal from "decimal.js";
 
 import { onConnect } from "./data-access";
-import { isUpsert, getValueFromChange, isDelete, isUpdate, isInsert } from "./changefeed";
+import { getValueFromChange } from "./changefeed";
 
 import { Transaction } from "./transaction.models";
-import { Student } from "./student.model";
-import { Account, AccountType, AccountValues } from "./account.model";
-import { calculateBalance, lastPaymentDate, TransactionService } from "./transaction.service";
+
 import { AccountService } from "./account.service";
-import { StudentService } from "./student.service";
+import { TransactionService } from "./transaction.service";
 
 onConnect(async (err, connection) => {
   console.log("transactionChangeFeed");
@@ -31,37 +29,5 @@ onConnect(async (err, connection) => {
       await accountService.updateAccountValues(account.id, { debit, credit });
     }
   });
-
-  /*txChangeFeed.each(async (err, change: r.Change<Transaction>) => {
-    console.log("change... ", i++);
-    console.log(change);
-
-    let { accountId } = getValueFromChange(change);
-    let account = await accountService.getAccount(accountId);
-
-    if (account) {
-      let { debit, credit } = AccountService.calculateAccountValues(account, change);
-      
-      await accountService.updateAccountValues(account.id, { debit, credit });
-    }
-  });*/
+  
 });
-
-
-/*cursor.eachAsync(
-  function (row, rowFinished) {
-      var ok = processRowData(row);
-      if (ok) {
-          rowFinished();
-      } else {
-          rowFinished('Bad row: ' + row);
-      }
-  },
-  function (error) {
-      if (error) {
-          console.log('Error:', error.message);
-      } else {
-          console.log('done processing');
-      }
-  }
-);*/
