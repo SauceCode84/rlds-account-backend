0214117efa0c6f097742accc80662cd07d99eeaa
SauceCode84 <rayruok@gmail.com>
2018-05-10 11:53:18 +0200

    refactored account routes

--- statement/account.routes.ts
+++ statement/account.routes.ts
@@ -9,53 +9,61 @@ export const accountsRouter = Router();
 
 type AccountServiceRequest = ServiceRequest<AccountService>;
 
-accountsRouter
-  .use(serviceRequestProvider(connection => new AccountService(connection)));
-
-accountsRouter.get("/", async (req: AccountServiceRequest, res: Response) => {
+const getAccounts = async (req: AccountServiceRequest, res: Response) => {
   let accounts = await req.service.getAccounts(req.query);
 
   res.json(accounts);
-});
+};
 
-accountsRouter.get("/names", async (req: AccountServiceRequest, res: Response) => {
+const getAccountNames = async (req: AccountServiceRequest, res: Response) => {
   let accountNames = await req.service.getAccountNames(req.query);
 
   res.json(accountNames);
-});
+};
 
-accountsRouter.get("/balances", async (req: AccountServiceRequest, res: Response) => {
+const getAccountBalances = async (req: AccountServiceRequest, res: Response) => {
   let accountBalances = await req.service.getAccountBalances();
 
   res.json(accountBalances);
-});
+};
 
-accountsRouter.get("/:id", async (req: AccountServiceRequest, res: Response) => {
+const getAccountById = async (req: AccountServiceRequest, res: Response) => {
   let { id } = req.params;
-
   let account = await req.service.getAccount(id);
 
   res.json(account);
-});
+};
 
-accountsRouter.get("/:id/subAccounts", async (req: AccountServiceRequest, res: Response) => {
+const getAccountSubAccounts = async (req: AccountServiceRequest, res: Response) => {
   let { id } = req.params;
 
   let subAccounts = await req.service.getSubAccounts(id);
 
   res.json(subAccounts);
-})
+};
 
-accountsRouter.post("/", async (req: AccountServiceRequest, res: Response) => {
+const postAccount = async (req: AccountServiceRequest, res: Response) => {
   await req.service.insertAccount(req.body);
 
   res.sendStatus(200);
-});
+};
 
-accountsRouter.put("/:id", async (req: AccountServiceRequest, res: Response) => {
+const putAccount = async (req: AccountServiceRequest, res: Response) => {
   let { id } = req.params;
 
   await req.service.updateAccount(id, req.body);
 
   res.sendStatus(200);
-});
+};
+
+accountsRouter
+  .use(serviceRequestProvider(connection => new AccountService(connection)));
+
+accountsRouter
+  .get("/", getAccounts)
+  .get("/names", getAccountNames)
+  .get("/balances", getAccountBalances)
+  .get("/:id", getAccountById)
+  .get("/:id/subAccounts", getAccountSubAccounts)
+  .post("/", postAccount)
+  .put("/:id", putAccount);
