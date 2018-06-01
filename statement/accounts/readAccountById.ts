f5342ed1bee8571bb5436f61efc545447ae815c2
SauceCode84 <rayruok@gmail.com>
2018-06-01 14:35:25 +0200

    readAccountById uses executeQuery

--- statement/accounts/readAccountById.ts
+++ statement/accounts/readAccountById.ts
@@ -2,20 +2,14 @@ import * as r from "rethinkdb";
 
 import { getConnection } from "../data-access";
 import { Account } from "../account.model";
+import { executeQuery } from "../executeQuery";
 
 const makeGetAccountById = (connection: r.Connection) => async (id: string): Promise<Account> => {
-  return r.table("accounts")
+  return await r.table("accounts")
     .get<Account>(id)
     .run(connection);
 }
 
 export type ReadAccountById = (id: string) => Promise<Account>;
 
-export const readAccountById = async (id: string): Promise<Account> => {
-  let connection = await getConnection();
-  let account = makeGetAccountById(connection)(id);
-  
-  await connection.close();
-
-  return account;
-}
+export const readAccountById: ReadAccountById = executeQuery(makeGetAccountById);
